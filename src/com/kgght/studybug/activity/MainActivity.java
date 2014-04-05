2e8204c10daa625271b077ce78901924fb6157c9
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-04-05 17:09:20 -0400

    complete failure

--- src/com/kgght/studybug/activity/MainActivity.java
+++ src/com/kgght/studybug/activity/MainActivity.java
@@ -1,29 +1,60 @@
 package com.kgght.studybug.activity;
 
+
+
 import com.kgght.studybug.R;
 
-import com.kgght.studybug.view.TitleView;
+import com.kgght.studybug.objects.CustomOnItemSelectedListener;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	  private Spinner spinner1;
+	  private Button btnSubmit;
+	 
+	  @Override
+	  public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
-		TitleView tView = new TitleView(this);
-		tView.setKeepScreenOn(true);
-		setContentView(tView);
+		setContentView(R.layout.activity_main);
+	 
+		addListenerOnButton();
+		addListenerOnSpinnerItemSelection();
+	  }
+	 
+	  // add items into spinner dynamically
+	  
+	  public void addListenerOnSpinnerItemSelection() {
+		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+	  }
+	 
+	  // get the selected dropdown list value
+	  public void addListenerOnButton() {
+	 
+		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		btnSubmit = (Button) findViewById(R.id.btnSubmit);
+	 
+		btnSubmit.setOnClickListener(new OnClickListener() {
+	 
+		  @Override
+		  public void onClick(View v) {
+	 
+		    Toast.makeText(MainActivity.this,
+			"OnClickListener : " + 
+	                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()), 
+				Toast.LENGTH_SHORT).show();
+		  }
+	 
+		});
+	  }
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-}
